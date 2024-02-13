1b82cd5b84c04f38f2007f29d54575108dd56e69
Humoyunmirzo <you@exaple.com>
2024-02-13 19:40:36 +0500

    first commit

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -4,13 +4,89 @@ import { NavLink } from "react-router-dom";
 function Navbar() {
   return (
     <div>
-      <div className="flex justify-around">
-        <NavLink to={"/"}>Home</NavLink>
-        <NavLink to={"/about"}>About</NavLink>
-        <NavLink to={"/clasess"}>Classes</NavLink>
-        <NavLink to={"/teachers"}>Teachers</NavLink>
-        <NavLink to={"/gallery"}>Gallary</NavLink>
-        <NavLink to={"/contact"}>Contact</NavLink>
+      <div className="flex justify-between items-center">
+        <div>
+          <div>
+            <p className="text-[#17a2b8] text-[50px] Kind">KidKinder</p>
+          </div>
+          <div></div>
+        </div>
+        <div className="flex gap-[30px]">
+          <NavLink
+            className={({ isActive, isPending }) =>
+              isPending
+                ? "pending"
+                : isActive
+                ? "text-[#17a2b8] font-bold Active"
+                : "NoActive"
+            }
+            to={"/"}
+          >
+            Home
+          </NavLink>
+          <NavLink
+            className={({ isActive, isPending }) =>
+              isPending
+                ? "pending"
+                : isActive
+                ? "text-[#17a2b8] font-bold Active"
+                : "NoActive"
+            }
+            to={"/about"}
+          >
+            About
+          </NavLink>
+          <NavLink
+            className={({ isActive, isPending }) =>
+              isPending
+                ? "pending"
+                : isActive
+                ? "text-[#17a2b8] font-bold Active"
+                : "NoActive"
+            }
+            to={"/clasess"}
+          >
+            Classes
+          </NavLink>
+          <NavLink
+            className={({ isActive, isPending }) =>
+              isPending
+                ? "pending"
+                : isActive
+                ? "text-[#17a2b8] font-bold Active"
+                : "NoActive"
+            }
+            to={"/teachers"}
+          >
+            Teachers
+          </NavLink>
+          <NavLink
+            className={({ isActive, isPending }) =>
+              isPending
+                ? "pending"
+                : isActive
+                ? "text-[#17a2b8] font-bold Active"
+                : "NoActive"
+            }
+            to={"/gallery"}
+          >
+            Gallary
+          </NavLink>
+
+          <NavLink
+            className={({ isActive, isPending }) =>
+              isPending
+                ? "pending"
+                : isActive
+                ? "text-[#17a2b8] font-bold Active"
+                : "NoActive"
+            }
+            to={"/contact"}
+          >
+            Contact
+          </NavLink>
+        </div>
+        <div></div>
       </div>
     </div>
   );
