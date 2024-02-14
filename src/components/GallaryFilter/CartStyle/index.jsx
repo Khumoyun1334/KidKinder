005744262255a06c5cbf725e8fde6a15d6b8ba9f
Timur <usmonaliyevtemur57@gmail.com>
2024-02-14 14:02:05 +0500

    men timur

--- src/components/GallaryFilter/CartStyle/index.jsx
+++ src/components/GallaryFilter/CartStyle/index.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function CartStyle({ img}) {
   return (
     <div>
-        <img src={img} className="w-[90%] m-auto bg-black p-2" alt="" />
+        <img src={img} className="w-[90%] m-auto" alt="" />
     </div>
   );
 }
